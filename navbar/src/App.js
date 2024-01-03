2687536203ae7ced31db65d5aac5ada9c706cfc2
himabindu1111 <himabindu1111@gmail.com>
2024-01-03 14:18:45 +0530

    initial navbar

---
+++ navbar/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Muinavbar from './components/Muinavbar';
+import MuiTogglebutton from './components/MuiTogglebutton';
+import MuiSelect from './components/MuiSelect';
+import Muicard from './components/Muicard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Muinavbar/>
+      <MuiTogglebutton/>
+      <MuiSelect/>
+      <Muicard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ navbar/src/components/MuiSelect.jsx
@@ -0,0 +1,21 @@
+
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material";
+
+const MuiSelect =()=>{
+    return (
+        <>
+        <FormControl width = "200px">
+     
+        <InputLabel id="select">Sort By</InputLabel>
+        <Select id="select" labelId="SortBy">
+
+        <MenuItem value={1}>Default</MenuItem>
+        <MenuItem value={2}>Price: high to low</MenuItem>
+        <MenuItem value={3}>Price: low to high</MenuItem>
+        <MenuItem value={4}>Newest</MenuItem>
+      </Select>
+      </FormControl>
+        </>
+);
+}
+export default MuiSelect;
